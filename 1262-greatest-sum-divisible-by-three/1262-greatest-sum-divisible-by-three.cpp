dae0b06d5e9f5d0cce989b27eb71383c17ec1285
sanatsawant <sanatsawantgvms@gmail.com>
2025-11-23 16:26:06 +0530

    Time: 0 ms (100%), Space: 29.3 MB (77.11%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
+    int dp[40005][3];
     int maxSumDivThree(vector<int>& v) {
         int n=v.size();
-        int a=1e9,b=1e9,c=1e9,d=1e9;
-        int ans=0;
-        for(auto x:v){
-            ans+=x;
-            if(x%3==1){
-                if(x<=a){
-                    b=a;
-                    a=x;
-                }
-                else if(x<=b) b=x;
-            }
-            else if(x%3==2){
-                if(x<=c){
-                    d=c;
-                    c=x;
-                }
-                else if(x<=d) d=x;
+     
+        for(int i = 0; i <= n; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                dp[i][j] =-1e9;
             }
         }
+        dp[0][0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<3;j++){
+                int nr=(j-v[i-1]%3+3)%3;
+                int take=v[i-1]+dp[i-1][nr];
+                int nt=dp[i-1][j];
+                dp[i][j]=max(take,nt);
+            }
+        }
+        return dp[n][0];
 
-        if(ans%3==1){
-            int k=0;
-            if(a!=-1) k=ans-a;
-            if(d!=-1) k=max(k,ans-c-d);
-            return k;
-        }
-        else if(ans%3==2){
-            int k=0;
-            if(c!=-1) k=ans-c;
-            if(b!=-1) k=max(k,ans-a-b);
-            return k;
-        }
-        return ans;
         
     }
 };
