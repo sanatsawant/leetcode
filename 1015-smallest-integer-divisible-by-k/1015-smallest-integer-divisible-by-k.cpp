f7884f9f118f50861d260f62c20496dd6fe79b53
sanatsawant <sanatsawantgvms@gmail.com>
2025-11-25 17:25:45 +0530

    Time: 4 ms (17.21%), Space: 7.9 MB (52.58%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -6,12 +6,9 @@ public:
         string n=string(to_string(k).size(),'1');
         long long a=stoi(n);
         ans=to_string(k).size();
-        unordered_set<int> st;
         for(int i=0;i<1e5;i++){
             a%=k;
             if(a==0) return ans;
-            else if(st.count(a)) return -1;
-            st.insert(a);
             a*=10;
             a+=1;
             ans++;
