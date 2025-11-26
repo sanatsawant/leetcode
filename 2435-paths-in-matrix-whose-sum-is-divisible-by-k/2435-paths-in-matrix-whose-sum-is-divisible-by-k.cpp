debdb18e32528cf6add27f75cde8054a5524d4f1
sanatsawant <sanatsawantgvms@gmail.com>
2025-11-26 14:08:38 +0530

    Time: 724 ms (5.21%), Space: 178.7 MB (13.98%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -10,7 +10,7 @@ public:
         auto check=[&](int i,int j){
             return i>=0 && j>=0 && i<n && j<m;
         };
-        function<int(int,int,int)> rec=[&](int i,int j,int cnt){
+        auto rec=[&](auto self,int i,int j,int cnt){
             if(i==n-1 && j==m-1 && (cnt+v[i][j])%k==0){
                 return 1;
             }
@@ -18,17 +18,17 @@ public:
                 int d=0,r=0;
                 int ncnt=(v[i][j]+cnt)%k;
                 if(check(i+1,j)){
-                    d=rec(i+1,j,ncnt);
+                    d=self(self,i+1,j,ncnt);
                 }
                 if(check(i,j+1)){
-                    r=rec(i,j+1,ncnt);
+                    r=self(self,i,j+1,ncnt);
                 }
                 dp[i][j][cnt]=(d%M+r%M)%M;
 
             }
             return dp[i][j][cnt];
         };
-        return rec(0,0,0);
+        return rec(rec,0,0,0);
         
     }
 };
