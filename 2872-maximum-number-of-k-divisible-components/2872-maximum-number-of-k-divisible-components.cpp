5107758b1c38fc0c7e2b0d03d13477136199e26d
sanatsawant <sanatsawantgvms@gmail.com>
2025-11-28 12:58:30 +0530

    Time: 230 ms (11.03%), Space: 195.8 MB (12.45%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<int> g[30005];
-    int vis[30005];
+    
     int ans=0;
-    int rec(int i,int k,vector<int>&v){
-        int r=0;
-        vis[i]=1;
-        for(auto child:g[i]){
-            if(vis[child]==0){
-                int rem=rec(child,k,v);
-                if(rem==0) ans++;
-                else{
-                    r+=rem;
-                    r%=k;
-                } 
-            }  
-        }
-      
-        return (v[i]+r)%k;
-    }
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        memset(vis,0,sizeof(vis));
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& v, int k) {
+        vector<vector<int>> g(n+1);
+        vector<int> vis(n+1);
         for(auto z:edges){
             int x=z[0];
             int y=z[1];
             g[x].push_back(y);
             g[y].push_back(x);
         }
-        int a=rec(0,k,values);
-        return ans+1;
+        auto rec=[&](auto self,int i)->int{
+            int r=0;
+            vis[i]=1;
+            for(auto child:g[i]){
+                if(vis[child]==0){
+                    int rem=self(self,child);
+                    r+=rem;
+                    r%=k;
+                    
+                }  
+            }
+            if((v[i]+r)%k == 0) ans++;
+            return (v[i]+r)%k;
+        };
+        int a=rec(rec,0);
+        return ans;
         
         
     }
