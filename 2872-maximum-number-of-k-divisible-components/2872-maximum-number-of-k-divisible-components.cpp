f984c3fa2067db89970f156830351676761c0edf
sanatsawant <sanatsawantgvms@gmail.com>
2025-11-28 13:28:51 +0530

    Time: 1177 ms (5.34%), Space: 187 MB (27.76%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -5,28 +5,42 @@ public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& v, int k) {
         vector<vector<int>> g(n+1);
         vector<int> vis(n+1);
+        vector<int> cnt(n+1);
         for(auto z:edges){
             int x=z[0];
             int y=z[1];
             g[x].push_back(y);
             g[y].push_back(x);
+            cnt[y]++;
+            cnt[x]++;
         }
-        auto rec=[&](auto self,int i)->int{
-            int r=0;
-            vis[i]=1;
-            for(auto child:g[i]){
-                if(vis[child]==0){
-                    int rem=self(self,child);
-                    r+=rem;
-                    r%=k;
+        cnt[0]++;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(cnt[i]==1) q.push(i);
+        }
+        while(!q.empty()){
+            int x=q.front();
+            cout<<x<<' '<<v[x]<<'\n';
+            q.pop();
+            if(v[x]%k==0) ans++;
+            if(cnt[x]>=1){
+                for(auto child:g[x]){
+                    if(cnt[child]>=1){
+                        v[child]+=v[x]%k;
+                        if(cnt[child]<=2) q.push(child);
+                        cnt[child]--;
+                        cnt[x]--;
+
+                    }
                     
-                }  
+                }
+
             }
-            if((v[i]+r)%k == 0) ans++;
-            return (v[i]+r)%k;
-        };
-        int a=rec(rec,0);
+            
+        }
         return ans;
+        
         
         
     }
