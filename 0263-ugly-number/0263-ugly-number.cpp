b1047368f67c563bbf2d33f5ccff96d9eae40ed7
sanatsawant <sanatsawantgvms@gmail.com>
2025-12-08 05:06:54 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45.42%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isUgly(int nn) {
+        if(nn<=0) return false;
+        long long n=nn;
+        
+        while(n%2==0) n/=2;
+        while(n%3==0) n/=3;
+        while(n%5==0) n/=5;
+        if(n!=1) return false;
+        return true;
+        
+    }
+};
